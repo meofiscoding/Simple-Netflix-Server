e53a572388cd19f544babb4a4b3c47670ffcf702
meof-coding <tranthe150186@fpt.edu.vn>
2023-09-07 19:28:04 +0700

    feat: add model and component for movie, remove un use image assets

---
+++ NetflixApp/src/app/models/movie.ts
@@ -0,0 +1,10 @@
+export class Movie{
+    id?: number;
+    backdrop_path?: string;
+    genres? : string[];
+    original_title?: string;
+    overview?: string;
+    poster_path?: string;
+    release_date?: string;
+    title?: string;
+}
