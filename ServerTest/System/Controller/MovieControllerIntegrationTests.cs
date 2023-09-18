e6418afb5e87e5d5066e5d21136371c983d1936d
meof-coding <tranthe150186@fpt.edu.vn>
2023-09-18 10:50:30 +0700

    fix: Fix assert in movie controller integration test

--- ServerTest/System/Controller/MovieControllerIntegrationTests.cs
+++ ServerTest/System/Controller/MovieControllerIntegrationTests.cs
@@ -2,6 +2,8 @@ using System;
 using System.Net;
 using SimpleServer.src.Movie;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using MongoConnector.Models;
 
 namespace SimpleServer.Test.System.Controller
 {
@@ -28,6 +30,19 @@ namespace SimpleServer.Test.System.Controller
 
             // Assert
             httpResponse.EnsureSuccessStatusCode(); // Status Code 200-299
+
+            // Deserialize and examine results.
+            var stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            var movies = JsonConvert.DeserializeObject<List<List<Movies>>>(stringResponse);
+            if(movies.Count > 0){
+                // if movie count is divisible by 5, then all chunk will have 5 elements
+                if(movies.Count % 5 == 0){
+                    movies.ForEach(movie => Assert.That(movie.Count, Is.EqualTo(5)));
+                }else{
+                    // if movie count is not divisible by 5, then last chunk will have less than 5 elements
+                    Assert.That(movies[movies.Count - 1].Count, Is.LessThan(5));
+                }
+            }
         }
     }
 }
