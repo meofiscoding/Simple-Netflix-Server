65467a65819ac2f5a27a7bb4cf2979e2954be6db
meof-coding <tranthe150186@fpt.edu.vn>
2023-10-04 20:38:00 +0700

    refactor code in Consts.cs

--- SimpleServer/src/Consts.cs
+++ SimpleServer/src/Consts.cs
@@ -5,12 +5,9 @@ namespace SimpleServer.src
     public class Consts
     {
         public const int UsernameMinLength = 5;
-
         public const string PasswordRegex = @"^(?=.*[A-Z])(?=.*[\W])(?=.*[0-9])(?=.*[a-z]).{6,128}$";
-
         public const string UsernameLengthValidationError = "Username must have more than 5 characters.";
         public const string EmailValidationError = "Email must have valid format.";
-
         public const string PasswordValidationError = "Password must have more than 6 characters, min. 1 uppercase, min. 1 lowercase, min. 1 special characters,min. 1 number,min.";
 
         public class LoginProviders
