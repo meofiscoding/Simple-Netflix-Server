9f2e22f78503dd54ad91787d196a0b1d03615343
meof-coding <tranthe150186@fpt.edu.vn>
2023-10-12 22:47:03 +0700

    update workflow

--- SimpleServer/src/Services/Identity/Identity.API/Entity/ApplicationUser.cs
+++ SimpleServer/src/Services/Identity/Identity.API/Entity/ApplicationUser.cs
@@ -3,8 +3,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Identity.API.Entity
 {
-    public class ApplicationUser: IdentityUser
+    public class ApplicationUser : IdentityUser
     {
-        
     }
 }
