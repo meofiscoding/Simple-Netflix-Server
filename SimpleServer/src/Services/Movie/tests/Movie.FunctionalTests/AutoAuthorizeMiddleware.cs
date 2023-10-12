3b53cb7714f12f3ee39a342b86e6115971d32cf0
meof-coding <tranthe150186@fpt.edu.vn>
2023-10-12 17:10:10 +0700

    add comment for AutoAuthorizeMiddleware class

--- SimpleServer/src/Services/Movie/tests/Movie.FunctionalTests/AutoAuthorizeMiddleware.cs
+++ SimpleServer/src/Services/Movie/tests/Movie.FunctionalTests/AutoAuthorizeMiddleware.cs
@@ -5,17 +5,21 @@ namespace Movie.FunctionalTests
 {
     class AutoAuthorizeMiddleware
     {
+        // GUID representing the identity of the user
         public const string IDENTITY_ID = "9e3163b9-1ae6-4652-9dc6-7898ab7b7a00";
 
         private readonly RequestDelegate _next;
 
+        // Configure the middleware and set the next step in the request pipeline.
         public AutoAuthorizeMiddleware(RequestDelegate rd)
         {
             _next = rd;
         }
 
+        // Automatically authorizes a user by attaching specific claims to their identity
         public async Task Invoke(HttpContext httpContext)
         {
+            // Simulate an authenticated user with the provided identity ID
             var identity = new ClaimsIdentity("cookies");
 
             identity.AddClaim(new Claim("sub", IDENTITY_ID));
@@ -24,6 +28,8 @@ namespace Movie.FunctionalTests
 
             httpContext.User.AddIdentity(identity);
 
+            // Ensures that the request continues to be processed by subsequent middleware components 
+            // after the claims have been added to the identity.
             await _next.Invoke(httpContext);
         }
     }
