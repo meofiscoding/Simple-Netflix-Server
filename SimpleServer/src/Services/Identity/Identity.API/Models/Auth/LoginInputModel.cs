73bd3b10718969bebb7d2880155648957e7de167
meof-coding <tranthe150186@fpt.edu.vn>
2023-10-18 16:25:30 +0700

    update login view model

--- SimpleServer/src/Services/Identity/Identity.API/Models/Auth/LoginInputModel.cs
+++ SimpleServer/src/Services/Identity/Identity.API/Models/Auth/LoginInputModel.cs
@@ -2,7 +2,7 @@ namespace Identity.API.Models.Auth;
 
 public class LoginInputModel
 {
-    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public string ReturnUrl { get; set; } = string.Empty;
 }
