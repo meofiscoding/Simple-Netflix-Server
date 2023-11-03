0935a2de13aa113724c281283d74a38033e3362b
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-03 18:41:15 +0700

    add unit test for crawl movie with type is series

--- SimpleServer/src/Services/Crawler/Test/Crawler.UnitTest/TestCrawlData.cs
+++ SimpleServer/src/Services/Crawler/Test/Crawler.UnitTest/TestCrawlData.cs
@@ -58,10 +58,43 @@ public class TestCrawlData
 
     [Test]
     // Testcase 1: MovieItem have category is Movies with nested query param in iframe src
-    [TestCase("https://phimmoiyyy.net/phim-le/ac-quy-ma-so-2775", "https://hd1080.opstream2.com/20231004/43610_67e76723/index.m3u8", "Ác Quỷ Ma Sơ 2 – The Nun 2 là bộ phim kinh dị siêu nhiên gothic của Hoa Kỳ được đạo diễn bởi Michael Chaves với kịch bản do Akela Cooper, Ian B. Goldberg và Richard Naing chấp bút dựa trên cốt truyện của Cooper và James Wan. Đây sẽ là phần hậu truyện của Ác quỷ ma sơ và đồng thời là phần phim thứ chín thuộc Vũ trụ The Conjuring.")]
+    [TestCase("https://phimmoiyyy.net/phim-bo/loki-phan-2-154230",
+              "Tập 5 Vietsub",
+              "https://hd1080.opstream2.com/20231004/43610_67e76723/index.m3u8",
+              "Loki: Phần 2 – Loki Season 2 (2023) khi Steve Rogers, Tony Stark và Scott Lang quay trở về cột mốc 2012, ngay khi trận chiến ở New York kết thúc, để “mượn tạm” quyền trượng của Loki. Nhưng một tai nạn bất ngờ xảy đến, khiến Loki nhặt được khối lặp phương Tesseract và tiện thể tẩu thoát.Cuộc trốn thoát này đã dẫn đến dòng thời gian bị rối loạn. Cục TVA – tổ chức bảo vệ tính nguyên vẹn của dòng chảy thời gian, buộc phải can thiệp, đi gô cổ ông thần này về làm việc. Tại đây, Loki có hai lựa chọn, một là giúp TVA ổn định lại thời gian, không thì bị tiêu hủy. Dĩ nhiên Loki chọn lựa chọn thứ nhất. Nhưng đây là nước đi vô cùng mạo hiểm, vì ông thần này thường lọc lừa, “lươn lẹo”, chuyên đâm lén như bản tính tự nhiên của gã.")]
     // Testcase 2: MovieItem have category is Movies without nested query param in iframe src
     // [TestCase("https://phimmoiyyy.net/phim-le/nam-dem-kinh-hoang", "https://cdn.helvid.com/video/m3u8/202311/030f13a212c5/playlist.m3u8", "Năm Đêm Kinh Hoàng – Five Nights at Freddy’s (2023) nhân viên bảo vệ Mike bắt đầu làm việc tại Freddy Fazbear’s Pizza. Trong đêm làm việc đầu tiên, anh nhận ra mình sẽ không dễ gì vượt qua được ca đêm ở đây. Chẳng mấy chốc, anh sẽ vén màn sự thật đã xảy ra tại Freddy’s.")]
-    public async Task TestCrawlMovieDetailAsync_MovieItemHaveCategoryIsTVShowsAndValidUrlDetail_ReturnMovieItemWithDescriptionAndStreamingUrlIsSetted(string inputUrl, string expectedStreamingUrl, string expectedDescription){
+    public async Task TestCrawlMovieDetailAsync_MovieItemHaveCategoryIsTVShowsAndValidUrlDetail_ReturnMovieItemWithDescriptionAndStreamingUrlIsSetted(string inputUrl, string status, string expectedStreamingUrl, string expectedDescription)
+    {
+        // Arrange
+        MovieItem movie = new()
+        {
+            MovieCategory = CrawlData.Enum.Category.TVShows,
+            UrlDetail = inputUrl,
+            Status = status
+        };
 
+        // Act
+        var result = await CrawlHelper.CrawlMovieDetailAsync(movie);
+
+        // Assert
+        List<string> streamingUrls = new(){
+            "https://hd1080.opstream2.com/20231006/43619_01b63816/index.m3u8",
+            "https://hd1080.opstream2.com/20231013/43761_81b78d84/index.m3u8",
+            "https://hd1080.opstream2.com/20231020/43783_004d4a5b/index.m3u8",
+            "https://hd1080.opstream2.com/20231027/43792_e1102d46/index.m3u8&id=50413",
+            "https://hd1080.opstream2.com/20231103/43830_fd67c109/index.m3u8"
+        };
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.StreamingUrls, Is.Not.Null);
+            // Description must not null
+            Assert.That(result.Description, Is.Not.Null);
+            // StreamingUrl must have number of item and each item must match by order with streamingUrls
+            Assert.That(result.StreamingUrls, Is.EqualTo(streamingUrls));
+            // Description must equal
+            Assert.That(result.Description, Is.EqualTo(expectedDescription));
+        });
     }
 }
