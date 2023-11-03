b581f62f9c0c372657b8cdf2b6d5494e0df7e9be
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-03 14:39:48 +0700

    feat: Scraping a Dynamic-Content

--- SimpleServer/src/Services/Crawler/CrawlData/Helper/CrawlHelper.cs
+++ SimpleServer/src/Services/Crawler/CrawlData/Helper/CrawlHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.RegularExpressions;
@@ -5,6 +6,9 @@ using System.Web;
 using CrawlData.Enum;
 using CrawlData.Model;
 using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace CrawlData.Helper
 {
@@ -114,44 +118,99 @@ namespace CrawlData.Helper
             {
                 throw new Exception($"Movie url of movie {movie.MovieName} is null or empty");
             }
-            HtmlDocument document = LoadDocument(movie.UrlDetail);
 
-            // get movie description from div which itemprop="description"
-            string? description = document.DocumentNode.Descendants("div")
-                .FirstOrDefault(x => x.GetAttributeValue("itemprop", "").Equals("description"))?.InnerText.Trim() ?? "";
-            movie.Description = HttpUtility.HtmlDecode(description);
-            // get movie streaming url
-            if (movie.MovieCategory == Category.Movies)
+            // to open Chrome in headless mode 
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+
+            // starting a Selenium instance 
+            using (var driver = new ChromeDriver(chromeOptions))
             {
-                // get the first iframe element
-                var iframe = document.DocumentNode.Descendants("iframe").FirstOrDefault();
-                var iframeSrc = iframe?.GetAttributeValue("src", "") ?? throw new Exception("Iframe src is null");
-                // check if link in src attribute contain ".m3u8"
-                if (iframeSrc.Contains(".m3u8"))
+                driver.Navigate().GoToUrl(movie.UrlDetail);
+                // wait until div with id playeroptions had no class "onload"
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+                wait.Until(x => x.FindElement(By.Id("playeroptions")).GetAttribute("class") != "onload");
+                // if timeout and div with id playeroptions still have class "onload" then throw exception
+                if (driver.FindElement(By.Id("playeroptions")).GetAttribute("class") == "onload")
                 {
-                    movie.StreamingUrl = iframe.GetAttributeValue("src", "");
+                    throw new Exception($"Iframe of movie {movie.MovieName} is not loaded");
+                }
+                // get movie description from div which itemprop="description"
+                string? description = driver.FindElement(By.CssSelector("div[itemprop='description']"))?.Text;
+                movie.Description = HttpUtility.HtmlDecode(description);
+                // get movie streaming url
+                if (movie.MovieCategory == Category.Movies)
+                {
+                    // get the first iframe element
+                    var iframe = driver.FindElement(By.TagName("iframe"));
+                    var iframeSrc = iframe.GetAttribute("src");
+                    // get all query params if exist in src attribute
+                    var queryParams = HttpUtility.ParseQueryString(iframeSrc);
+                    // Case 1: Not have query param: 
+                    // If iframe src contain .m3u8 then set streaming url = decode iframe src
+                    // Else call HTTP GET to get the streaming url
+                    // Case 2: Have query param:
+                    // Filter all query param to get value which value start with "http"
+                    // Decode url
+                    // If url contain query param which value contain .m3u8 then set streaming url = url
+                    // Else call HTTP GET to get the streaming url
+                    if (queryParams.Count == 0)
+                    {
+                        if (iframeSrc.Contains(".m3u8"))
+                        {
+                            movie.StreamingUrl = HttpUtility.UrlDecode(iframeSrc);
+                        }
+                        else
+                        {
+                            // call HTTP GET to get the streaming url
+                            movie.StreamingUrl = await GetStreamingUrlAsync(iframeSrc);
+                        }
+                    }
+                    else
+                    {
+                        var url = queryParams.AllKeys
+                            .Where(x => queryParams[x].StartsWith("http"))
+                            .Select(x => HttpUtility.UrlDecode(queryParams[x]))
+                            .FirstOrDefault() ?? throw new Exception("No .m3u8 url found");
+
+                        // Get the final url in query param until not contain query param
+                        while (url.Contains('?'))
+                        {
+                            // get all query params if exist in url
+                            queryParams = HttpUtility.ParseQueryString(url);
+                            url = queryParams.AllKeys
+                                .Where(x => queryParams[x].StartsWith("http"))
+                                .Select(x => HttpUtility.UrlDecode(queryParams[x]))
+                                .FirstOrDefault() ?? throw new Exception("No .m3u8 url found");
+                        }
+
+                        if (url.Contains(".m3u8"))
+                        {
+                            movie.StreamingUrl = url;
+                        }
+                        else
+                        {
+                            // call HTTP GET to get the streaming url
+                            movie.StreamingUrl = await GetStreamingUrlAsync(url);
+                        }
+                    }
                 }
                 else
                 {
-                    // get all query params in src attribute
-                    var queryParams = HttpUtility.ParseQueryString(iframeSrc) ?? throw new Exception("Query params is null");
-                    // Filter all query param to get value which value start with "http"
-                    var url = Array.Find(queryParams.AllKeys, x => queryParams[x].StartsWith("http")) ?? throw new Exception("Not found url in query params");
-                    // Decode url
-                    url = HttpUtility.UrlDecode(url);
-                    // call HTTP GET to get the streaming url
-                    var response = await HttpHelper.GetAsync(url);
-                    // get the url contain .m3u8 in response, url must start with "http"
-                    var streamingUrl = StreamingUrlRegex().Match(response).Value;
-                    movie.StreamingUrl = streamingUrl;
+                    // movie.StreamingUrl = document.DocumentNode.Descendants("iframe")
+                    //     .FirstOrDefault(x => x.GetAttributeValue("id", "").Equals("iframe-embed"))?.GetAttributeValue("data-src", "");
                 }
             }
-            else
-            {
-                movie.StreamingUrl = document.DocumentNode.Descendants("iframe")
-                    .FirstOrDefault(x => x.GetAttributeValue("id", "").Equals("iframe-embed"))?.GetAttributeValue("data-src", "");
-            }
+
             return movie;
+        }
+
+        private static async Task<string> GetStreamingUrlAsync(string url)
+        {
+            var response = await HttpHelper.GetAsync(url);
+            // get the url contain .m3u8 in response, url must start with "http"
+            var streamingUrl = StreamingUrlRegex().Match(response).Value;
+            return HttpUtility.UrlDecode(streamingUrl);
         }
 
         [GeneratedRegex("http.*?\\.m3u8")]
