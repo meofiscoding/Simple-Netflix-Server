92b57a7d5df6d18780f971054894af9dd52dbbd2
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-06 20:31:48 +0700

    Update MovieItem.cs

--- SimpleServer/src/Services/Crawler/CrawlData/Model/MovieItem.cs
+++ SimpleServer/src/Services/Crawler/CrawlData/Model/MovieItem.cs
@@ -1,18 +1,21 @@
 using CrawlData.Enum;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace CrawlData.Model
 {
     public class MovieItem
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
         public string MovieName { get; set; } = string.Empty;
 
         public string? Poster { get; set; }
 
         public string Status { get; set; } = "";
 
-        public Dictionary<int, string> StreamingUrls { get; set; } = new Dictionary<int, string>();
-
-        public string? TrailerUrl { get; set; }
+        public Dictionary<string, string> StreamingUrls { get; set; } = new Dictionary<string, string>();
 
         public string? UrlDetail { get; set; }
 
@@ -22,7 +25,8 @@ namespace CrawlData.Model
 
         public List<Tag> Tags { get; set; } = new List<Tag>();
 
-        // keep track all remain episode that didn't upload to GCS
-        public List<int> RemainEpisodes { get; set; } = new List<int>();
+        public bool IsAvailable { get; set; }
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
     }
 }
