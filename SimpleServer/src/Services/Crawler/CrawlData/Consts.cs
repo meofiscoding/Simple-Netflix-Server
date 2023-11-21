263268de3307854e569f4c9e8ecd56cab99424ef
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-21 23:26:28 +0700

    update number of movie to push each day

--- SimpleServer/src/Services/Crawler/CrawlData/Consts.cs
+++ SimpleServer/src/Services/Crawler/CrawlData/Consts.cs
@@ -5,7 +5,7 @@ namespace CrawlData
     public static class Consts
     {
         public const string BUCKET_NAME = "simple-netflix";
-        public const int NUMBER_OF_MOVIE_TO_PUSH_EACH_DAY = 3;
+        public const int NUMBER_OF_MOVIE_TO_PUSH_EACH_DAY = 5;
         public const int ACCEPTABLE_ERROR = 2;
         public static string MOVIE_WEBSITE_URL = "https://phimmoiyyy.net/";
     }
