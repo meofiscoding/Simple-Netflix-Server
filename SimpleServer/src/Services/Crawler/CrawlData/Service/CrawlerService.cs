f12e143909cd0453fbb1680139763c07639428be
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-21 17:53:27 +0700

    update logic to crawl data

--- SimpleServer/src/Services/Crawler/CrawlData/Service/CrawlerService.cs
+++ SimpleServer/src/Services/Crawler/CrawlData/Service/CrawlerService.cs
@@ -41,37 +41,53 @@ namespace CrawlData.Service
 
             // CRAWL MOVIE DETAILs
             // loop until tvShowsWithFullNonNullStreamingUrls have an element that its number of streamingUrls + moviesWithNonNullStreamingUrls.Count = numberOfMoviesToPushEachDay
-            while (!tvShowsWithFullNonNullStreamingUrls.Any(tvShow => tvShow.StreamingUrls.Count + moviesWithNonNullStreamingUrls.Count >= Consts.NUMBER_OF_MOVIE_TO_PUSH_EACH_DAY))
+            // while (!tvShowsWithFullNonNullStreamingUrls.Any(tvShow => tvShow.StreamingUrls.Count + moviesWithNonNullStreamingUrls.Count >= Consts.NUMBER_OF_MOVIE_TO_PUSH_EACH_DAY))
+            // {
+            //     foreach (var movie in movies)
+            //     {
+            //         // get movie from _database that has the same name as the movie
+            //         var movieFromDB = await _database.GetMovieByNameAsync(movie.MovieName);
+            //         var result = await CrawlHelper.CrawlMovieDetailAsync(movieFromDB ?? movie);
+            //         // add the movie to the _database
+            //         await _database.UpsertMovieAsync(result);
+            //         Console.WriteLine($"Movie {result.MovieName} added to the _database successfully!!");
+            //     }
+            //     // get all movies from the _database
+            //     movies = await _database.GetAllMovie();
+            //     tvShowsWithFullNonNullStreamingUrls = MovieHelper.GetMoviesWithStreamingUrls(movies, Category.TVShows);
+            // }
+
+            // CRAWL MOVIE DETAILs Logic Updated
+            foreach (var movie in movies)
             {
-                foreach (var movie in movies)
-                {
-                    // get movie from _database that has the same name as the movie
-                    var movieFromDB = await _database.GetMovieByNameAsync(movie.MovieName);
-                    var result = await CrawlHelper.CrawlMovieDetailAsync(movieFromDB ?? movie);
-                    // add the movie to the _database
-                    await _database.UpsertMovieAsync(result);
-                    Console.WriteLine($"Movie {result.MovieName} added to the _database successfully!!");
-                }
-                // get all movies from the _database
-                movies = await _database.GetAllMovie();
+                // get movie from _database that has the same name as the movie
+                var movieFromDB = await _database.GetMovieByNameAsync(movie.MovieName);
+                var result = await CrawlHelper.CrawlMovieDetailAsync(movieFromDB ?? movie);
+                // add the movie to the _database
+                await _database.UpsertMovieAsync(result);
+                Console.WriteLine($"Movie {result.MovieName} added to the _database successfully!!");
                 moviesWithNonNullStreamingUrls = MovieHelper.GetMoviesWithStreamingUrls(movies, Category.Movies);
                 tvShowsWithFullNonNullStreamingUrls = MovieHelper.GetMoviesWithStreamingUrls(movies, Category.TVShows);
+                if(tvShowsWithFullNonNullStreamingUrls.Any(tvShow => tvShow.StreamingUrls.Count + moviesWithNonNullStreamingUrls.Count >= Consts.NUMBER_OF_MOVIE_TO_PUSH_EACH_DAY))
+                {
+                    break;
+                }
             }
 
             // PUSH MOVIE ASSET TO GCS
             // Only push 5 item of movie to GCS in each day
             // Movie can be both in Movies and TVShows category
-            // if (moviesWithNonNullStreamingUrls.Count == 0 && tvShowsWithFullNonNullStreamingUrls.Count == 0)
-            // {
-            //     Log.Error("No movie to push to GCS today!");
-            // }
-            // else
-            // {
-            //     var moviesToPushToGCS = MovieHelper.GetMoviesToPushToGCS(moviesWithNonNullStreamingUrls, tvShowsWithFullNonNullStreamingUrls);
-            //     var result = await MovieHelper.PushMovieAssetToGCS(moviesToPushToGCS, _database);
-            //     // Send event to MassTransit-RabbitMQ
-            //     await PublishMoviesAsync(result);
-            // }
+            if (moviesWithNonNullStreamingUrls.Count == 0 && tvShowsWithFullNonNullStreamingUrls.Count == 0)
+            {
+                Log.Error("No movie to push to GCS today!");
+            }
+            else
+            {
+                var moviesToPushToGCS = MovieHelper.GetMoviesToPushToGCS(moviesWithNonNullStreamingUrls, tvShowsWithFullNonNullStreamingUrls);
+                var result = await MovieHelper.PushMovieAssetToGCS(moviesToPushToGCS, _database);
+                // Send event to MassTransit-RabbitMQ
+                await PublishMoviesAsync(result);
+            }
             Console.WriteLine("Done crawling movie data!");
         }
 
