80dc65e5e02f3867bd2ab4ad876385ea37f60c65
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-24 20:07:35 +0700

    add api for movie

--- SimpleServer/src/Services/Movie/Movie.API/Controllers/MovieController.cs
+++ SimpleServer/src/Services/Movie/Movie.API/Controllers/MovieController.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Net;
+using EventBus.Message.Common.Enum;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Movie.API.Models;
@@ -11,8 +12,8 @@ using Movie.API.Repository;
 
 namespace SimpleServer.src.Movie;
 
-[Authorize]
-[Route("api/movie")]
+[Authorize(Roles = "Member")]
+[Route("api/")]
 public class MovieController : Controller
 {
     private readonly IMovieRepository _movieService;
@@ -24,11 +25,52 @@ public class MovieController : Controller
         _logger = logger;
     }
 
-    // GET: api/values
-    [HttpGet]
-    [ProducesResponseType(typeof(IEnumerable<MovieInformation>), (int)HttpStatusCode.OK)]
-    public async Task<ActionResult<List<List<MovieInformation>>>> GetAllMoviesAsync()
+    // Get all category
+    [HttpGet("categories")]
+    [ProducesResponseType(typeof(IEnumerable<string>), (int)HttpStatusCode.OK)]
+    public ActionResult<Dictionary<int, string>> GetAllCategories()
     {
-        return Ok(await _movieService.GetAllMoviesAsync());
+        return Ok(_movieService.GetAllCategories());
+    }
+
+    // Get category by id
+    [HttpGet("category/{id}")]
+    [ProducesResponseType(typeof(IEnumerable<MovieInformation>), (int)HttpStatusCode.OK)]
+    public ActionResult<string> GetCategoryById(int id)
+    {
+        string category = ((Category)id).ToString();
+        return Ok(new { category });
+    }
+
+    // Seach movie
+    [HttpGet("movies/search")]
+    [ProducesResponseType(typeof(MovieSearchResult), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<MovieSearchResult>> SearchMovies([FromQuery] MovieSearchQueryModel queryParams)
+    {
+        return Ok(await _movieService.SearchMoviesAsync(queryParams));
+    }
+
+    // Get All Tags
+    [HttpGet("tags")]
+    [ProducesResponseType(typeof(IEnumerable<string>), (int)HttpStatusCode.OK)]
+    public ActionResult<List<string>> GetAllTags()
+    {
+        return Ok(_movieService.GetAllTags());
+    }
+
+    // GET: api/values
+    [HttpGet("tag/{tag}")]
+    [ProducesResponseType(typeof(IEnumerable<MovieInformation>), (int)HttpStatusCode.OK)]
+    public ActionResult<List<List<MovieInformation>>> GetAllMovies(string tag)
+    {
+        return Ok(_movieService.GetAllMoviesByTag(tag));
+    }
+
+    // GET api/values/5
+    [HttpGet("movies/{id}")]
+    [ProducesResponseType(typeof(MoviePlayerModel), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<MoviePlayerModel>> GetMovieById(string id)
+    {
+        return Ok(await _movieService.GetMovieByIdAsync(id));
     }
 }
