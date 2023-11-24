bd56e2f1c5985a5478719f8aba7413e342fb73d7
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-24 20:04:58 +0700

    add search and get movie data logic

--- SimpleServer/src/Services/Movie/Movie.API/Repository/IMovieRepository.cs
+++ SimpleServer/src/Services/Movie/Movie.API/Repository/IMovieRepository.cs
@@ -1,11 +1,17 @@
 using System;
+using EventBus.Message.Common.Enum;
 using Movie.API.Models;
 
 namespace Movie.API.Repository
 {
     public interface IMovieRepository
     {
-        Task AddMovieAsync(MovieInformation movie);
-        public Task<List<List<MovieInformation>>> GetAllMoviesAsync(); 
+        Task UpsertMovieAsync(MovieInformation movie);
+        Dictionary<int,string> GetAllCategories();
+        public List<MovieInformation> GetAllMoviesByCategory(int category);
+        List<List<MovieInformation>> GetAllMoviesByTag(string tag);
+        Task<MoviePlayerModel> GetMovieByIdAsync(string id);
+        List<string> GetAllTags();
+        Task<MovieSearchResult> SearchMoviesAsync(MovieSearchQueryModel queryModel);
     }
 }
