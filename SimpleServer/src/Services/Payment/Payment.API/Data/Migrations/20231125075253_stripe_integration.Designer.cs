63c7780d34475cdd22c7e18827ad9979e5f970c4
meof-coding <tranthe150186@fpt.edu.vn>
2023-11-25 15:30:46 +0700

    migrate database

--- SimpleServer/src/Services/Payment/Payment.API/Data/Migrations/20231125075253_stripe_integration.Designer.cs
+++ SimpleServer/src/Services/Payment/Payment.API/Data/Migrations/20231125075253_stripe_integration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Payment.API.Data;
@@ -11,9 +12,11 @@ using Payment.API.Data;
 namespace Payment.API.Data.Migrations
 {
     [DbContext(typeof(PaymentDBContext))]
-    partial class PaymentDBContextModelSnapshot : ModelSnapshot
+    [Migration("20231125075253_stripe_integration")]
+    partial class stripe_integration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SimpleServer/src/Services/Payment/Payment.API/Data/Migrations/20231125075253_stripe_integration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Payment.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class stripe_integration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StripePriceId",
+                table: "Subcriptions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StripeProductId",
+                table: "Subcriptions",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StripePriceId",
+                table: "Subcriptions");
+
+            migrationBuilder.DropColumn(
+                name: "StripeProductId",
+                table: "Subcriptions");
+        }
+    }
+}
